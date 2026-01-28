beca8a62d2b852202f67ef6713a7a75d56dc2b3e
LorinHan <627075866@qq.com>
2026-01-28 17:23:12 +0800

    feat: version update

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 )
 
 func main() {
-	fmt.Println("webkit v1.0.9")
+	fmt.Println("webkit v1.1.0")
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("请输入项目名称（默认'test_webkit'）：")
 	input, err := reader.ReadString('\n')
